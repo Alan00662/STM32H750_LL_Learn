952b66a5363530d6cf0a86dc9921ac108bfb5f95
Alan <alan_rd@radiomasterrc.com>
2026-01-09 11:09:20 +0800

    feat:添加boot Cmake工程

---
+++ TX15_Boot_Cmake/Core/Inc/main.h
@@ -0,0 +1,91 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define ENTER_Pin GPIO_PIN_12
+#define ENTER_GPIO_Port GPIOG
+#define MDL_Pin GPIO_PIN_3
+#define MDL_GPIO_Port GPIOE
+#define LEDR_Pin GPIO_PIN_8
+#define LEDR_GPIO_Port GPIOI
+#define PAGE_R_Pin GPIO_PIN_8
+#define PAGE_R_GPIO_Port GPIOA
+#define LEDB_Pin GPIO_PIN_10
+#define LEDB_GPIO_Port GPIOI
+#define LEDG_Pin GPIO_PIN_11
+#define LEDG_GPIO_Port GPIOI
+#define PAGE_L_Pin GPIO_PIN_7
+#define PAGE_L_GPIO_Port GPIOG
+#define RTN_Pin GPIO_PIN_3
+#define RTN_GPIO_Port GPIOG
+#define SYS_Pin GPIO_PIN_2
+#define SYS_GPIO_Port GPIOB
+#define POWER_ON_Pin GPIO_PIN_12
+#define POWER_ON_GPIO_Port GPIOH
+#define POWER_SWITCH_Pin GPIO_PIN_4
+#define POWER_SWITCH_GPIO_Port GPIOA
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
